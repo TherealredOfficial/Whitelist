923b5b54742b9a27ee68b3c447c2e3ab553539ca
Spectre <148616802+TherealredOfficial@users.noreply.github.com>
2023-12-21 21:34:36 +0200

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,1 +1,13 @@
+-- Whitelist.lua
+
+local Whitelisted = {}
+
+-- Function to check if a user ID is whitelisted
+function isUserWhitelisted(userId)
+    return Whitelisted[userId] or false
+end
+
+return {
+    Whitelisted = Whitelisted,
+}
 
